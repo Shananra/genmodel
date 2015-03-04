e6a7769c53fbd08556fba9e0123d7011e20bcf7d
Adam Weber <aweber@stampinup.com>
2015-03-04 11:37:46 -0700

    updates to support sqlalchemy as well as django

--- genmodel/genmodel.py
+++ genmodel/genmodel.py
@@ -29,6 +29,8 @@ try:
 except ValueError:
     print(Fore.RED+"Error: Missing required setting CONNECTION in settings flie\n"+Fore.RESET,file=sys.stderr)
     exit()
+if not hasattr(settings,'dialect') or settings.dialect == None:
+    settings.dialect = 'sqlalchemy'
 
 engine = sqlalchemy.create_engine(myconn)
 engine.connect()
@@ -62,8 +64,8 @@ def run_table(tablename):
     if int('excludes' in mytables[tablename]) != int('includes' in mytables[tablename]) > 1:
         print(Fore.RED+"The table "+tablename+" has both an excludes and includes parameter."+Fore.RESET+"\nOnly one such parameter can be processed on the same table.\n",file=sys.stderr)
         exit()
-    
-    output += "\nclass %(tablename)s(models.Model):\n" % {'tablename':tablename}
+    base = 'models.Model' if settings.dialect == 'django' else 'base'
+    output += "\nclass %(tablename)s(%(base)s):\n" % {'tablename':tablename,'base':base}
     if hasattr(settings,'METHODS'):
         output += settings.METHODS+"\n"
     if 'methods' in mytables[tablename]:
@@ -80,25 +82,46 @@ def run_table(tablename):
         if 'primary_key' in mytables[tablename]:
             if item[0] == mytables[tablename]['primary_key']:
                 pkey=',primary_key=True'
-        output += "    %(fieldname)s =  %(fvalue)s\n" % {'fieldname':item[0],'fvalue':{
-            'varchar':"models.CharField(max_length=%(length)s,blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
-            'nvarchar':"models.CharField(max_length=%(length)s,blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
-            'int':"models.IntegerField(blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
-            'decimal':"models.DecimalField(max_digits=%(length)s,decimal_places=4,blank=True%(pkey)s)" % {'length':int(item[2]) + 4,'pkey':pkey},
-            'money':"models.DecimalField(max_digits=%(length)s,decimal_places=2,blank=True%(pkey)s)" % {'length':int(item[2])+2,'pkey':pkey},
-            'bit':'models.BooleanField(blank=True)',
-            'smallint':'models.IntegerField(blank=True)',
-            'tinyint':'models.IntegerField(blank=True)',
-            'datetime':'models.DateTimeField(blank=True)',
-            'char':"models.CharField(max_length=%(length)s,blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
-            'nchar':"models.CharField(max_length=%(length)s,blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey}
-        }[item[1]]} 
+        if settings.dialect == 'django':
+            output += "    %(fieldname)s =  %(fvalue)s\n" % {'fieldname':item[0],'fvalue':{
+                'varchar':"models.CharField(max_length=%(length)s,blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
+                'nvarchar':"models.CharField(max_length=%(length)s,blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
+                'int':"models.IntegerField(blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
+                'decimal':"models.DecimalField(max_digits=%(length)s,decimal_places=4,blank=True%(pkey)s)" % {'length':int(item[2]) + 4,'pkey':pkey},
+                'money':"models.DecimalField(max_digits=%(length)s,decimal_places=2,blank=True%(pkey)s)" % {'length':int(item[2])+2,'pkey':pkey},
+                'bit':'models.BooleanField(blank=True)',
+                'smallint':'models.IntegerField(blank=True)',
+                'tinyint':'models.IntegerField(blank=True)',
+                'datetime':'models.DateTimeField(blank=True)',
+                'char':"models.CharField(max_length=%(length)s,blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
+                'nchar':"models.CharField(max_length=%(length)s,blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey}
+            }[item[1]]}
+        elif settings.dialect == 'sqlalchemy':
+            output += "    %(fieldname)s =  %(fvalue)s\n" % {'fieldname':item[0],'fvalue':{
+                'varchar':"String(max_length=%(length)s,nullable=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
+                'nvarchar':"Unicode(max_length=%(length)s,nullable=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
+                'int':"Integer(blank=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
+                'decimal':"DECIMAL(max_digits=%(length)s,nullable=True%(pkey)s)" % {'length':int(item[2]) + 4,'pkey':pkey},
+                'money':"Numeric(max_digits=%(length)s,nullable=True%(pkey)s)" % {'length':int(item[2])+2,'pkey':pkey},
+                'bit':'Boolean(nullable=True)',
+                'smallint':'Integer(nullable=True)',
+                'tinyint':'Integer(nullable=True)',
+                'datetime':'DateTime(nullable=True)',
+                'char':"CHAR(length=%(length)s,nullable=True%(pkey)s)" % {'length':item[2],'pkey':pkey},
+                'nchar':"Unicode(length=%(length)s,nullable=True%(pkey)s)" % {'length':item[2],'pkey':pkey}
+            }[item[1]]}
         
 
 def main():
     global output
     
-    output = "from django.db import models\n"
+    if settings.dialect == 'django':
+        output = "from django.db import models\n"
+    elif settings.dialect == 'sqlalchemy':
+        output = "from sqlalchemy.types import * \n"
+    else:
+        print(Fore.RED+"Unknown dialect: "+settings.dialect+Fore.RESET,file=sys.stderr)
+    
     try:
         output += settings.FILE_HEADER
     except ValueError:
